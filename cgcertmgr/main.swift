ce9ca1adac5da23394c594f6dc83c1bbf5dfb3f3
alfred_eisenberg <alfred.eisenberg@quest.com>
2024-07-10 14:19:35 -0400

    update with right main remove userstate stuff

--- cgcertmgr/main.swift
+++ cgcertmgr/main.swift
@@ -1,38 +1,43 @@
-import Foundation
-import Security
+//
+//  main.swift
+//  cgcertmgr
+//
+//  Created by Alfred Eisenberg on 7/9/24.
+//
 
 import Foundation
 import Security
 
-let certPEM = """
------BEGIN CERTIFICATE-----
-MIIEJjCCAw6gAwIBAgIQZwBinnGe0eANbPsdiAW1WDANBgkqhkiG9w0BAQsFADBM
-MQswCQYDVQQGEwJVUzEMMAoGA1UECgwDUFlBMS8wLQYDVQQDDCZCbHVlQXJtb3It
-UG9ydGFibGUgU3ViIENBIHY0LjAgZm9yIFRBQzAeFw0yNDA2MjcxNDE2NTRaFw0y
-NTA2MjcxNDE2NTRaMGUxCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJOSDESMBAGA1UE
-CgwJSW52aXNpbmV0MQwwCgYDVQQLDANlcGMxJzAlBgNVBAMMHnRlc3RkZWxldGVq
-dW5lMjcuaW52aXNpbmV0LmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
-ggEBANHiYbDqNFSAgkUD4EIZ15HS24wFV9n7EZ5qYczOd/pKO5CqWbnlab0i8/KK
-FU8j4DxD1rKrkymFLB3uUMO2qIRAodSdSZlwOq9oRjCiqBsHfHz88dEouArh5pAz
-qBFzkBIcTESVd5Vi+j1vc2XU4CraANhQ7XamjsLjlcju6lGexxASBdyGMhe3ob7b
-cWYVtQmrp9OO2Y08hbCcxLzV2SMj6OT3J6pSN7phPhzAjF/bgJ9HHYvvDiYTl6SR
-QmOaueR0npl45RvPPo2C+W2MBRr0bIoznXqXNxMdwzgfQjXBDN0jq4qNX5TWLVMj
-tQIdg6jRdewJXx2Vqhec8eolfI0CAwEAAaOB6jCB5zAJBgNVHRMEAjAAMAsGA1Ud
-DwQEAwIFoDAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwbgYDVR0RBGcw
-ZYIedGVzdGRlbGV0ZWp1bmUyNy5pbnZpc2luZXQuY29thkNibHVlYXJtb3I6Ly90
-ZXN0ZGVsZXRlanVuZTI3LmludmlzaW5ldC5jb20vQWdlbnQvaWRlbnRpdHlBY3Rp
-dmF0aW9uMB0GA1UdDgQWBBQP1+OCUAgsPoK5+jBIrOpyVGPzRzAfBgNVHSMEGDAW
-gBSJWYXB8Kj3AXsnSvieggSaDrdyOzANBgkqhkiG9w0BAQsFAAOCAQEAuJql/GlA
-RK+nAHPJqgRc4aaMeVEL/kn4taOpSL5UyagTrDWawgXnRVAt+s0i/evbjqUK3n/T
-XbW5xS1W6IN67xwm5BARNEPOow8GhvniWZLDvHIkA/Mth51RH+uGLB2PAvP3pKP8
-f81Pp1rc093xAvqx4lnwIoT15FjN6o+sLGhKdEUJr75UGvQxo2FLSq2XEnWMaK64
-u/HD6OwVCQZkY0i+8t1Zy7RsPpsXWXFNyjdVSgdM7+/RSwNtdNEiB61o2XsqmNSu
-kQLwXMrgilZ/5VFnvXkbTG0DFQyOpsMA6eByKNBLOgWhN8ZtOreqORe7FsE0GAsu
-A76X5eeqmTS4Pw==
------END CERTIFICATE-----
-"""
-
-let keyPEM = """
+// Example usage
+let pemCertificate = """
+ -----BEGIN CERTIFICATE-----
+ MIIEJjCCAw6gAwIBAgIQZwBinnGe0eANbPsdiAW1WDANBgkqhkiG9w0BAQsFADBM
+ MQswCQYDVQQGEwJVUzEMMAoGA1UECgwDUFlBMS8wLQYDVQQDDCZCbHVlQXJtb3It
+ UG9ydGFibGUgU3ViIENBIHY0LjAgZm9yIFRBQzAeFw0yNDA2MjcxNDE2NTRaFw0y
+ NTA2MjcxNDE2NTRaMGUxCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJOSDESMBAGA1UE
+ CgwJSW52aXNpbmV0MQwwCgYDVQQLDANlcGMxJzAlBgNVBAMMHnRlc3RkZWxldGVq
+ dW5lMjcuaW52aXNpbmV0LmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+ ggEBANHiYbDqNFSAgkUD4EIZ15HS24wFV9n7EZ5qYczOd/pKO5CqWbnlab0i8/KK
+ FU8j4DxD1rKrkymFLB3uUMO2qIRAodSdSZlwOq9oRjCiqBsHfHz88dEouArh5pAz
+ qBFzkBIcTESVd5Vi+j1vc2XU4CraANhQ7XamjsLjlcju6lGexxASBdyGMhe3ob7b
+ cWYVtQmrp9OO2Y08hbCcxLzV2SMj6OT3J6pSN7phPhzAjF/bgJ9HHYvvDiYTl6SR
+ QmOaueR0npl45RvPPo2C+W2MBRr0bIoznXqXNxMdwzgfQjXBDN0jq4qNX5TWLVMj
+ tQIdg6jRdewJXx2Vqhec8eolfI0CAwEAAaOB6jCB5zAJBgNVHRMEAjAAMAsGA1Ud
+ DwQEAwIFoDAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwbgYDVR0RBGcw
+ ZYIedGVzdGRlbGV0ZWp1bmUyNy5pbnZpc2luZXQuY29thkNibHVlYXJtb3I6Ly90
+ ZXN0ZGVsZXRlanVuZTI3LmludmlzaW5ldC5jb20vQWdlbnQvaWRlbnRpdHlBY3Rp
+ dmF0aW9uMB0GA1UdDgQWBBQP1+OCUAgsPoK5+jBIrOpyVGPzRzAfBgNVHSMEGDAW
+ gBSJWYXB8Kj3AXsnSvieggSaDrdyOzANBgkqhkiG9w0BAQsFAAOCAQEAuJql/GlA
+ RK+nAHPJqgRc4aaMeVEL/kn4taOpSL5UyagTrDWawgXnRVAt+s0i/evbjqUK3n/T
+ XbW5xS1W6IN67xwm5BARNEPOow8GhvniWZLDvHIkA/Mth51RH+uGLB2PAvP3pKP8
+ f81Pp1rc093xAvqx4lnwIoT15FjN6o+sLGhKdEUJr75UGvQxo2FLSq2XEnWMaK64
+ u/HD6OwVCQZkY0i+8t1Zy7RsPpsXWXFNyjdVSgdM7+/RSwNtdNEiB61o2XsqmNSu
+ kQLwXMrgilZ/5VFnvXkbTG0DFQyOpsMA6eByKNBLOgWhN8ZtOreqORe7FsE0GAsu
+ A76X5eeqmTS4Pw==
+ -----END CERTIFICATE-----
+ """
+	
+let pemKey = """
 -----BEGIN PRIVATE KEY-----
 MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDR4mGw6jRUgIJF
 A+BCGdeR0tuMBVfZ+xGeamHMznf6SjuQqlm55Wm9IvPyihVPI+A8Q9ayq5MphSwd
@@ -63,179 +68,179 @@ S8PypMwjZDMS7p+uemTpfg==
 -----END PRIVATE KEY-----
 """
 
-// Helper function to convert PEM to SecCertificate
-func convertPEMToSecCertificate(pemString: String) -> SecCertificate? {
-	let base64String = pemString.replacingOccurrences(of: "-----BEGIN CERTIFICATE-----", with: "")
-								.replacingOccurrences(of: "-----END CERTIFICATE-----", with: "")
-								.replacingOccurrences(of: "\n", with: "")
-	guard let certData = Data(base64Encoded: base64String) else {
-		print("Failed to base64 decode certificate data")
-		return nil
-	}
-	guard let certificate = SecCertificateCreateWithData(nil, certData as CFData) else {
-		print("Failed to create SecCertificate from data")
-		return nil
-	}
-	return certificate
+
+func convertPEMToSecCertificate(_ pemContent: String) -> SecCertificate? {
+	guard let data = extractPEMSection(pemContent: pemContent, section: "CERTIFICATE") else { return nil }
+	return SecCertificateCreateWithData(nil, data as CFData)
 }
 
-// Helper function to convert PEM to SecKey
-func convertPEMToSecKey(pemString: String) -> SecKey? {
-	// Remove headers and footers, and any newlines
-	let base64String = pemString
-		.replacingOccurrences(of: "-----BEGIN PRIVATE KEY-----", with: "")
-		.replacingOccurrences(of: "-----END PRIVATE KEY-----", with: "")
-		.replacingOccurrences(of: "\n", with: "")
-	
-	// Decode base64 string into Data
-	guard let keyData = Data(base64Encoded: base64String) else {
-		print("Failed to base64 decode private key data")
+func convertPEMToSecKey(_ pemContent: String) -> SecKey? {
+	guard let data = extractPEMSection(pemContent: pemContent, section: "PRIVATE KEY") else { return nil }
+	return createPrivateKey(from: data)
+}
+
+func extractPEMSection(pemContent: String, section: String) -> Data? {
+	let startMarker = "-----BEGIN \(section)-----"
+	let endMarker = "-----END \(section)-----"
+
+	guard let startRange = pemContent.range(of: startMarker),
+		  let endRange = pemContent.range(of: endMarker) else {
+		print("Markers not found")
 		return nil
 	}
+
+	var base64Content = pemContent[startRange.upperBound..<endRange.lowerBound]
+		.trimmingCharacters(in: .whitespacesAndNewlines)
 	
-	// Create attributes dictionary
-	let keyDict: [CFString: Any] = [
-		kSecAttrKeyType: kSecAttrKeyTypeRSA,
-		kSecAttrKeyClass: kSecAttrKeyClassPrivate,
-		kSecAttrKeySizeInBits: 2048 as CFNumber
+	base64Content = base64Content.replacingOccurrences(of: "\n", with: "")
+	base64Content = base64Content.replacingOccurrences(of: "\r", with: "")
+
+	print("Extracted base64 content: \(base64Content)")
+
+	guard let data = Data(base64Encoded: base64Content) else {
+		print("Failed to decode base64")
+		return nil
+	}
+
+	return data
+}
+
+func createPrivateKey(from data: Data) -> SecKey? {
+	let options: [NSString: Any] = [kSecAttrKeyType: kSecAttrKeyTypeRSA, kSecAttrKeyClass: kSecAttrKeyClassPrivate]
+	return SecKeyCreateWithData(data as CFData, options as CFDictionary, nil)
+}
+
+func addCertificateToKeychain(certificate: SecCertificate, label: String) -> Bool {
+	let addQuery: [NSString: Any] = [
+		kSecClass: kSecClassCertificate,
+		kSecAttrLabel: label,
+		kSecValueRef: certificate
 	]
-	
-	// Attempt to create SecKey from data
-	var error: Unmanaged<CFError>?
-	guard let secKey = SecKeyCreateWithData(keyData as CFData, keyDict as CFDictionary, &error) else {
-		if let error = error?.takeRetainedValue() {
-			print("Failed to create SecKey from data: \(error)")
-		} else {
-			print("Failed to create SecKey from data")
+
+	let status = SecItemAdd(addQuery as CFDictionary, nil)
+	if status != errSecSuccess {
+		print(SecCopyErrorMessageString(status, nil) as String? ?? "Unknown error")
+	}
+	return status == errSecSuccess
+}
+
+func addPrivateKeyToKeychain(privateKey: SecKey, label: String) -> Bool {
+	let addQuery: [NSString: Any] = [
+		kSecClass: kSecClassKey,
+		kSecAttrKeyClass: kSecAttrKeyClassPrivate,
+		kSecAttrLabel: label,
+		kSecValueRef: privateKey
+	]
+
+	let status = SecItemAdd(addQuery as CFDictionary, nil)
+	if status != errSecSuccess {
+		if status == errSecDuplicateItem {
+			print("Key already exists: errSecDuplicateItem")
 		}
+		print(SecCopyErrorMessageString(status, nil) as String? ?? "Unknown error")
+	}
+	return status == errSecSuccess
+}
+
+func findCertificateInKeychain(label: String) -> SecCertificate? {
+	let query: [NSString: Any] = [
+		kSecClass: kSecClassCertificate,
+		kSecAttrLabel: label,
+		kSecReturnRef: kCFBooleanTrue!,
+		kSecMatchLimit: kSecMatchLimitOne
+	]
+
+	var item: CFTypeRef?
+	let status = SecItemCopyMatching(query as CFDictionary, &item)
+	print("findCertificateInKeychain status: \(status)")
+	if status != errSecSuccess {
+		print(SecCopyErrorMessageString(status, nil) as String? ?? "Unknown error")
+	}
+	guard status == errSecSuccess, let certificate = item else {
+		print("Certificate not found")
 		return nil
 	}
-	
-	return secKey
+	return (certificate as! SecCertificate)
 }
 
-func addCertificateToKeychain(cert: SecCertificate) -> OSStatus {
-	let addQuery: [String: Any] = [kSecClass as String: kSecClassCertificate,
-								   kSecValueRef as String: cert,
-								   kSecAttrLabel as String: "MyIdentityCertificate"]
-	
-	let status = SecItemAdd(addQuery as CFDictionary, nil)
+func findPrivateKeyInKeychain(label: String) -> SecKey? {
+	let query: [NSString: Any] = [
+		kSecClass: kSecClassKey,
+		kSecAttrKeyClass: kSecAttrKeyClassPrivate,
+		kSecAttrLabel: label,
+		kSecReturnRef: kCFBooleanTrue!,
+		kSecMatchLimit: kSecMatchLimitOne
+	]
+
+	var item: CFTypeRef?
+	let status = SecItemCopyMatching(query as CFDictionary, &item)
+	print("findPrivateKeyInKeychain status: \(status)")
 	if status != errSecSuccess {
-		print("Failed to add certificate to keychain: \(status)")
+		print(SecCopyErrorMessageString(status, nil) as String? ?? "Unknown error")
 	}
-	return status
+	guard status == errSecSuccess, let privateKey = item else {
+		print("Private key not found")
+		return nil
+	}
+	return (privateKey as! SecKey)
 }
 
-func addKeyToKeychain(key: SecKey) -> OSStatus {
-	let addQuery: [String: Any] = [kSecClass as String: kSecClassKey,
-								   kSecAttrApplicationTag as String: "MyIdentityKey",
-								   kSecValueRef as String: key]
-	
-	let status = SecItemAdd(addQuery as CFDictionary, nil)
+func deleteCertificateFromKeychain(label: String) -> Bool {
+	let deleteQuery: [NSString: Any] = [
+		kSecClass: kSecClassCertificate,
+		kSecAttrLabel: label
+	]
+
+	let status = SecItemDelete(deleteQuery as CFDictionary)
 	if status != errSecSuccess {
-		print("Failed to add key to keychain: \(status)")
+		print(SecCopyErrorMessageString(status, nil) as String? ?? "Unknown error")
 	}
-	return status
+	return status == errSecSuccess
 }
 
-func findIdentityInKeychain() -> (SecCertificate?, SecKey?) {
-	let certQuery: [String: Any] = [kSecClass as String: kSecClassCertificate,
-									kSecAttrLabel as String: "MyIdentityCertificate",
-									kSecReturnRef as String: kCFBooleanTrue!,
-									kSecMatchLimit as String: kSecMatchLimitOne]
-	
-	var certItem: CFTypeRef?
-	let certStatus = SecItemCopyMatching(certQuery as CFDictionary, &certItem)
-	if certStatus != errSecSuccess {
-		print("Failed to find certificate in keychain: \(certStatus)")
+func deletePrivateKeyFromKeychain(label: String) -> Bool {
+	let deleteQuery: [NSString: Any] = [
+		kSecClass: kSecClassKey,
+		kSecAttrKeyClass: kSecAttrKeyClassPrivate,
+		kSecAttrLabel: label
+	]
+
+	let status = SecItemDelete(deleteQuery as CFDictionary)
+	if status != errSecSuccess {
+		print(SecCopyErrorMessageString(status, nil) as String? ?? "Unknown error")
 	}
-	
-	let keyQuery: [String: Any] = [kSecClass as String: kSecClassKey,
-								   kSecAttrApplicationTag as String: "MyIdentityKey",
-								   kSecReturnRef as String: kCFBooleanTrue!,
-								   kSecMatchLimit as String: kSecMatchLimitOne]
-	
-	var keyItem: CFTypeRef?
-	let keyStatus = SecItemCopyMatching(keyQuery as CFDictionary, &keyItem)
-	if keyStatus != errSecSuccess {
-		print("Failed to find key in keychain: \(keyStatus)")
-	}
-	
-	guard certStatus == errSecSuccess, let certificate = certItem as! SecCertificate?,
-		  keyStatus == errSecSuccess, let privateKey = keyItem as! SecKey? else {
-		return (nil, nil)
-	}
-	
-	return (certificate, privateKey)
+	return status == errSecSuccess
 }
 
-func deleteIdentityFromKeychain() -> (OSStatus, OSStatus) {
-	let certQuery: [String: Any] = [kSecClass as String: kSecClassCertificate,
-									kSecAttrLabel as String: "MyIdentityCertificate"]
-	
-	let certStatus = SecItemDelete(certQuery as CFDictionary)
-	if certStatus != errSecSuccess {
-		print("Failed to delete certificate from keychain: \(certStatus)")
+func tryCerts(pemCertificate: String) {
+	if let certificate = convertPEMToSecCertificate(pemCertificate)  {
+		let certificateAdded = addCertificateToKeychain(certificate: certificate, label: "com.example.mycert")
+		if certificateAdded {
+			print("Certificate added: \(certificateAdded)")
+			let foundCertificate = findCertificateInKeychain(label: "com.example.mycert")
+			print("Certificate found: \(String(describing: foundCertificate))")
+			let certificateDeleted = deleteCertificateFromKeychain(label: "com.example.mycert")
+			print("Certificate deleted: \(certificateDeleted)")
+		}
 	}
-	
-	let keyQuery: [String: Any] = [kSecClass as String: kSecClassKey,
-								   kSecAttrApplicationTag as String: "MyIdentityKey"]
-	
-	let keyStatus = SecItemDelete(keyQuery as CFDictionary)
-	if keyStatus != errSecSuccess {
-		print("Failed to delete key from keychain: \(keyStatus)")
+}
+
+func tryKeys(pemKey: String) {
+	if let privateKey = convertPEMToSecKey(pemKey) {
+		let keyAdded = addPrivateKeyToKeychain(privateKey: privateKey, label: "com.example.mykey")
+		if keyAdded {
+			print("Key added: \(keyAdded)")
+			if let foundPrivateKey = findPrivateKeyInKeychain(label: "com.example.mykey") {
+				print("Private key found: \(foundPrivateKey)")
+				let keyDeleted = deletePrivateKeyFromKeychain(label: "com.example.mykey")
+				print("Private key deleted: \(keyDeleted)")
+			}
+		}
 	}
+}
+
 	
-	return (certStatus, keyStatus)
-}
-
-
-// Add key
-if let privateKey = convertPEMToSecKey(pemString: keyPEM) {
-	print("Private key successfully converted to SecKey")
-	// Optionally, you can print the SecKey for debugging
-	print("Private Key: \(privateKey)")
-	let keyAddStatus = addKeyToKeychain(key: privateKey)
-
-	if keyAddStatus == errSecSuccess {
-		print("Private key successfully added to keychain")
-	} else {
-		print("Failed to add private key to keychain")
-	}
-} else {
-	print("Failed to convert private key to SecKey")
-}
-
-// Add cert
-if let certificate = convertPEMToSecCertificate(pemString: certPEM) {
-	print("Certificate successfully converted to SekCertificate")
-	// Optionally, you can print the SecKey for debugging
-	print("Certificate: \(certificate)")
+tryCerts(pemCertificate: pemCertificate)
 	
-	let certAddStatus = addCertificateToKeychain(cert: certificate)
+tryKeys(pemKey: pemKey)
 
-	if certAddStatus == errSecSuccess {
-		print("Certificate successfully added to keychain")
-	} else {
-		print("Failed to add Certificate to keychain")
-	}
-} else {
-	print("Failed to convert private key to SecKey")
-}
-	
-
-// Finding the certificate and key in the keychain
-let (foundCertificate, foundKey) = findIdentityInKeychain()
-if let cert = foundCertificate, let key = foundKey {
-	print("Certificate and Key found in keychain")
-} else {
-	print("Certificate or Key not found in keychain")
-}
-
-// Deleting the certificate and key from the keychain
-let (certDeleteStatus, keyDeleteStatus) = deleteIdentityFromKeychain()
-if certDeleteStatus == errSecSuccess, keyDeleteStatus == errSecSuccess {
-	print("Certificate and Key successfully deleted from keychain")
-} else {
-	print("Failed to delete Certificate or Key from keychain")
-}
